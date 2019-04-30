29ac41f03aaaf2402c64661d88559f630f7a6099
tolik1106 <tolik1106@i.ua>
2019-04-30 10:59:11 +0300

    Start_with_Spring_Boot

---
+++ src/main/java/com/zhitar/topjavagraduation/TopjavaGraduationApplication.java
@@ -0,0 +1,13 @@
+package com.zhitar.topjavagraduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopjavaGraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopjavaGraduationApplication.class, args);
+    }
+
+}
